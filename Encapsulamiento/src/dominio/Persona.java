d863d70614674bd22cbdecb8c7ad48e789655849
Danwenvega <76652993+Danwenvega@users.noreply.github.com>
2022-04-05 17:39:36 -0500

    editando variables

---
+++ Encapsulamiento/src/dominio/Persona.java
@@ -0,0 +1,47 @@
+
+package dominio;
+
+public class Persona {
+    private String name;
+    private String fired;
+    private double salary;
+    private boolean eliminated;
+    
+    public Persona (String name, double salary, boolean eliminated ){
+        this.name = name;
+        this.salary = salary;
+        this.eliminated = eliminated;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public double getSalary(){
+        return this.salary;
+    }
+    
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+    
+    public boolean isEliminated(){
+        return this.eliminated;
+    }
+    
+    public void getEliminated(boolean eliminated){
+        this.eliminated = eliminated;
+    }
+    
+    public String getFired(){
+        return this.fired;
+    }
+    
+    public void setFired(String fired){
+        this.fired = fired;
+    }
+}
