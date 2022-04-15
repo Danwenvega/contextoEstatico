da483d9d5f0c5a68842948c9a6e8117dd1f89d74
Danwenvega <76652993+Danwenvega@users.noreply.github.com>
2022-04-14 20:53:14 -0500

    Agregando Id y prueba de contador

---
+++ contextoEstatico/src/test/PersonaPrueba.java
@@ -0,0 +1,14 @@
+
+package test;
+
+import domain.Persona;
+
+public class PersonaPrueba {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan");
+        System.out.println("persona1 = " + persona1);
+        
+        Persona person2 = new Persona ("Karla");
+        System.out.println("person2 = " + person2);
+    }
+}
